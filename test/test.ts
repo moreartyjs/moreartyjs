485320b2fa21bad5ad6106d65ca91eaea61d28b0
mbektimirov <mbektimirov@gmail.com>
2014-11-16 17:12:26 +0500

    writing types

--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,6 @@
-/// <reference path="../node_modules/immutable/dist/immutable.d.ts" />
 /// <reference path="../type-definitions/morearty.d.ts" />
-
-//import M = module('morearty');
 
 import M = require('morearty');
 
-//var b = new Binding();
+
+var b = new M.Binding();
--- type-definitions/morearty.d.ts
+++ type-definitions/morearty.d.ts
@@ -1,45 +1,62 @@
 /// <reference path="../node_modules/immutable/dist/immutable.d.ts" />
 
-import I = require('immutable');
-
 declare module 'morearty' {
+  import I = require('immutable');
+  import Map = I.Map;
+  import Iterable = I.Iterable;
+
+  interface ContextConfig {
+    requestAnimationFrameEnabled: boolean;
+  }
+
+  interface IMergeStrategy {
+    OVERWRITE: string;
+    OVERWRITE_EMPTY: string;
+    MERGE_PRESERVE: string;
+    MERGE_REPLACE: string;
+  }
 
   class Context {
-    constructor(initialState: I.Map<any, any>, initialMetaState: I.Map<any, any>, configuration: Object);
+    constructor(initialState: Map<any, any>, initialMetaState: Map<any, any>, configuration: ContextConfig);
     getBinding(): Binding;
     getMetaBinding(): Binding;
-    getCurrentState(): I.Map<any, any>;
-    getPreviousState(): I.Map<any, any>;
-    getCurrentMeta(): I.Map<any, any>;
-    getPreviousMeta(): I.Map<any, any>;
+    getCurrentState(): Map<any, any>;
+    getPreviousState(): Map<any, any>;
+    getCurrentMeta(): Map<any, any>;
+    getPreviousMeta(): Map<any, any>;
     resetState(subpath: string, options: Object);
     resetState(subpath: string[], options: Object);
-    replaceState(newState: I.Map<any, any>, newMetaState: I.Map<any, any>, options: {notify: boolean});
-    isChanged(binding: Binding, subpath: string, compare: (val1: I.Iterable<any, any>, val2: I.Iterable<any, any>) => boolean);
-    isChanged(binding: Binding, subpath: string[], compare: (val1: I.Iterable<any, any>, val2: I.Iterable<any, any>) => boolean);
+    replaceState(newState: Map<any, any>, newMetaState: Map<any, any>, options: {notify: boolean});
+    isChanged(binding: Binding, subpath: string, compare: (val1: Iterable<any, any>, val2: Iterable<any, any>) => boolean);
+    isChanged(binding: Binding, subpath: string[], compare: (val1: Iterable<any, any>, val2: Iterable<any, any>) => boolean);
     init(rootComp: Object);
     queueFullUpdate();
   }
 
   export interface Mixin {
-    //contextTypes: {morearty: () => Context};
+    contextTypes: {morearty: () => Context};
     getMoreartyContext(): Context;
     getBinding(name: string): Binding;
     getBinding(name: string): Object;
     getDefaultBinding(): Binding;
-    getPreviousState(): I.Iterable<any, any>;
+    getPreviousState(): Iterable<any, any>;
     componentWillMount();
     shouldComponentUpdate(nextProps: Object, nextState: Object);
     addBindingListener(binding: Binding, subpath: string, cb: (c: ChangesDescriptor) => void)
+    addBindingListener(binding: Binding, subpath: string[], cb: (c: ChangesDescriptor) => void)
     componentWillUnmount();
   }
 
-  export module Binding {
+  export var MergeStrategy: IMergeStrategy;
 
-  }
+  export function createContext(
+    initialState: Map<any,any>,
+    initialMetaState: Map<any,any>,
+    options: ContextConfig
+  ): Context;
 
   export class Binding {
-
+    constructor(backingValueHolder, metaBinding, path, options, internals);
   }
 
   export module Util {
