7268af3e77e9c653462315f32ad2ecd58c44bba4
Alexander Semenov <tvaroh@icloud.com>
2015-08-05 13:22:31 +0300

    Dist 0.7.24.

--- dist/morearty.js
+++ dist/morearty.js
@@ -1,13 +1,10 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Morearty = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-(function (global){
-var React = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null);
+var React = (window.React);
 var DOM = require('./src/DOM');
 module.exports = require('./src/Morearty')(React, DOM);
 
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./src/DOM":4,"./src/Morearty":6}],2:[function(require,module,exports){
-(function (global){
-var Imm = (typeof window !== "undefined" ? window['Immutable'] : typeof global !== "undefined" ? global['Immutable'] : null);
+var Imm = (window.Immutable);
 var Util = require('./Util');
 var ChangesDescriptor = require('./ChangesDescriptor');
 
@@ -904,7 +901,6 @@ TransactionContext.prototype = (function () {
 
 module.exports = Binding;
 
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./ChangesDescriptor":3,"./Util":7}],3:[function(require,module,exports){
 var Util = require('./Util');
 
@@ -1010,9 +1006,8 @@ ChangesDescriptor.prototype = {
 module.exports = ChangesDescriptor;
 
 },{"./Util":7}],4:[function(require,module,exports){
-(function (global){
 var Util  = require('./Util');
-var React = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null);
+var React = (window.React);
 
 var _ = (function() {
   if (React) return React.DOM;
@@ -1071,10 +1066,8 @@ var DOM = {
 
 module.exports = DOM;
 
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./Util":7}],5:[function(require,module,exports){
-(function (global){
-var Imm = (typeof window !== "undefined" ? window['Immutable'] : typeof global !== "undefined" ? global['Immutable'] : null);
+var Imm = (window.Immutable);
 var Binding = require('./Binding');
 
 var getHistoryBinding, initHistory, clearHistory, destroyHistory, listenForChanges, revertToStep, revert;
@@ -1235,15 +1228,13 @@ var History = {
 
 module.exports = History;
 
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./Binding":2}],6:[function(require,module,exports){
-(function (global){
 /**
  * @name Morearty
  * @namespace
  * @classdesc Morearty main module. Exposes [createContext]{@link Morearty.createContext} function.
  */
-var Imm      = (typeof window !== "undefined" ? window['Immutable'] : typeof global !== "undefined" ? global['Immutable'] : null);
+var Imm      = (window.Immutable);
 var Util     = require('./Util');
 var Binding  = require('./Binding');
 var History  = require('./History');
@@ -1754,7 +1745,7 @@ module.exports = function (React, DOM) {
         },
 
         render: function () {
-          var effectiveProps = Util.assign({}, this.props, {binding: ctx.getBinding()});
+          var effectiveProps = Util.assign({}, this.props, { binding: ctx.getBinding() });
 
           return React.createFactory(rootComp)(effectiveProps);
         }
@@ -2008,7 +1999,6 @@ module.exports = function (React, DOM) {
   };
 };
 
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./Binding":2,"./History":5,"./Util":7,"./util/Callback":8}],7:[function(require,module,exports){
 /**
  * @name Util
